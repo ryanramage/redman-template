8089d9888a739c386eff00d25d2e801a3d5c5376
Ryan Ramage <ryan.ramage@gmail.com>
2015-07-29 14:03:26 -0600

    move to standard, format, add docker info

--- test/unit.js
+++ test/unit.js
@@ -1,9 +1,8 @@
-var mod = require('../lib/index');
-
-var test = require('tape');
-
+var mod = require('../lib/index')
+var test = require('tape')
 
 test('fill in this', function (t) {
+  mod()
   t.ok(true)
-  t.end();
-});
+  t.end()
+})
